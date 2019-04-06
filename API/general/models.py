5150bc9b1f28f69e07b2a1a802a3bed09fa79e74
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2019-04-06 10:46:13 -0700

    track run algorithm when call apis

--- API/general/models.py
+++ API/general/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class RunAlgorithm(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     input = models.TextField()
     output = models.TextField()
     run_at = models.DateTimeField()
