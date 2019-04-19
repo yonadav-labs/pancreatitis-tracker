ed9e43db38b04dffce709d6cc189fee998529908
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2019-04-19 02:36:31 -0700

    add load historic input data

--- API/apsc/urls.py
+++ API/apsc/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('run_algorithms', views.run_algorithms),
     path('run_algorithm/<str:algorithm>', views.run_algorithm),
     path('algorithms', views.get_algorithms),
+    path('load_input_history', views.load_input_history),
 
     path('register', auth.register),
     path('verify_email/<str:jwt_code>', auth.verify_email, name='verify_email')
--- API/general/auth.py
+++ API/general/auth.py
@@ -15,7 +15,7 @@ def register(request):
     """
     body format: { "name": "John Doe", "email": "some@email.com" } 
     """
-    data = json.loads(request.body)
+    data = json.loads(request.body.decode("ascii"))
     user = User.objects.filter(email=data['email']).first()
     jwt_code = jwt.encode({ 'email' : data['email'] }, settings.SECRET_KEY, algorithm='HS256').decode('ascii')
 
@@ -27,7 +27,7 @@ def register(request):
     elif user and not user.is_active:
         res = {
             "status": "unverified",
-            "msg": "Please verify your email"
+            "msg": "Please verify your email."
         }
     else:
         names = data['name'].split(' ')
@@ -43,7 +43,7 @@ def register(request):
         send_email([data['email']], 'Welcome to APSC', email_body)
         res = {
             "status": "email-sent",
-            "msg": "Please check your email"
+            "msg": "Verification email sent. Please check your email."
         }
 
     return JsonResponse(res, safe=True)
