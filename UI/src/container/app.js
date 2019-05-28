eef09b7d67ee9bd58ba6de7ae67090c8241a49ad
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2019-05-28 01:29:20 -0700

    feat: add google analytics

--- UI/src/container/app.js
+++ UI/src/container/app.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import RootComponent from "./root";
+import ReactGA from 'react-ga';
 import "react-toastify/dist/ReactToastify.min.css";
 import "./app.scss";
+
+function initializeReactGA() {
+	ReactGA.initialize('UA-140942097-1');
+	ReactGA.pageview('/patient');
+}
 
 const App = () => {
 	return (
